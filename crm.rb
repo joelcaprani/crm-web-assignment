91eeb9b79ba93e769540c95052f6a1112de4aab9
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-24 18:41:38 -0500

    contacts work

--- crm.rb
+++ crm.rb
@@ -15,6 +15,20 @@ end
 
 
 
+get '/' do
+  erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+
+
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
