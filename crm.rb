bb61ae2c81bea4b0ac1ff2e9fa90770164b0e6ac
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-24 17:05:11 -0500

    first commit

--- crm.rb
+++ crm.rb
@@ -1,3 +1,9 @@
+require 'sinatra'
+
+get '/' do
+  'Main Menu'
+end
+
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
