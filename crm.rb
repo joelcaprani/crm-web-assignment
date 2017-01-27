77f1b429e5d05d67c61cecaad52fabadcc5b86af
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-27 16:25:08 -0500

    final commit, test done

--- crm.rb
+++ crm.rb
@@ -1,19 +1,6 @@
 require 'sinatra'
 require_relative 'contact'
 
-Contact.create('Joel', 'Caprani', 'joel@joel.com', 'Nice guy')
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@microsoft.com', 'Visionary')
-Contact.create('Bill', 'Gates', 'bill@apple.com', 'crazy man')
-Contact.create('Marshall', 'Mathers', 'm&m@apple.com', 'rapper')
-Contact.create('Slim', 'Shady', 'm&m@rap.com', 'poet')
-Contact.create('Dr', 'Dre', 'dre@aftermath.com', 'Producer')
-Contact.create('Paul', 'Newman', 'paul@cool.com', 'actor')
-Contact.create('Mick', 'Jagger', 'mick@rolling.com', 'singer')
-
-
-
 
 
 
@@ -33,10 +20,20 @@ get '/contacts/new' do
   erb :new_contact
 end
 
+# post '/contacts' do
+# Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+# redirect to ('/contacts')
+# end
 post '/contacts' do
-Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-redirect to ('/contacts')
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
 end
+
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
